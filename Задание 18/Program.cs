a716835ea7c0dcc015cc496077cf2dd19ce52cf3
Илья Смирнов <cyber-shot3333@yandex.ru>
2022-01-10 16:56:49 +0300

    Добавьте файлы проекта.

---
+++ Задание 18/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку, содержащую круглые, квадратные и фигурные скобки");
+            string s = Console.ReadLine();
+            bool b = true;
+            Stack<char> bracket = new Stack<char>();
+            foreach (char i in s)
+            {
+                if (i == '(')
+                {
+                    bracket.Push(')');
+                }
+                if (i == '[')
+                {
+                    bracket.Push(']');
+                }
+                if (i == '{')
+                {
+                    bracket.Push('}');
+                }
+                if (bracket.Count > 0 && i == bracket.Peek())
+                {
+                    bracket.Pop();
+                    continue;
+                }
+                if (i == ')' || i == ']' || i == '}' && bracket.Count == 0)
+                {
+                    b = false;
+                }
+            }
+            if (bracket.Count == 0 && b == true)
+            {
+                Console.WriteLine("Скобки расставлены корректно");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены не корректно");
+            }
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
